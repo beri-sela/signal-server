e75f4d32aa5190c3ed3ff2f54bdbf43bf0fabe92
berisela <63008028+beri-sela@users.noreply.github.com>
2021-11-06 12:37:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ mv ~/signal-server ~/signal
 sudo apt-get install -y nginx
 cd ~/signal && sudo docker-compose up -d && \
 cd ~/signal && git clone https://github.com/signalapp/signal/Signal-Server.git
-cd ~/signal/Signal-Server and git checkout v4.97
+cd ~/signal/Signal-Server && git checkout v4.97
 cd ~/signal/Signal-Server && mvn -DskipTests package
 # sudo docker run -d --restart unless-stopped --name accountdb -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=accountdb" -p 5432:5432 postgres:11 && \
 # sudo docker run -d --restart unless-stopped --name abusedb -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=abusedb" -p 5433:5432 postgres:11  && \
