add084da9f06fc521b65227d67550652bf770c75
berisela <63008028+beri-sela@users.noreply.github.com>
2021-11-06 12:30:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cd
 sudo apt-get update -y
 sudo apt install -y git maven
-sudo apt-get install -y docker docker.io
+sudo apt-get install -y docker docker.io docker-compose
 sudo git clone https://github.com/beri-sela/signal-server
 mv ~/signal-server ~/signal
 sudo apt-get install -y nginx
@@ -19,7 +19,7 @@ java -jar ~/signal/Signal-Server/service/target/TextSecureServer-4.97.jar messag
 # sudo docker run -d --restart unless-stopped --name redis -e "IP=0.0.0.0" -p 7000-7005:7000-7005 grokzen/redis-cluster:latest
 # sudo docker run -d --restart unless-stopped --name nginx --net="host" -v /home/ubuntu/nginx/nginx.conf:/etc/nginx/nginx.conf:ro nginx
 cd
-echo "Open config.yml in signal/Signal-Server and make any changes there then run: \n
-nohup java -jar ~/signal/Signal-Server/service/target/TextSecureServer-4.97.jar server ~/signal/Signal-Server/config.yml \n 
-OR as below as daemon \n
+echo "Open config.yml in signal/Signal-Server and make any changes there then run: 
+java -jar ~/signal/Signal-Server/service/target/TextSecureServer-4.97.jar server ~/signal/Signal-Server/config.yml
+OR as below as daemon
 nohup java -jar ~/signal/Signal-Server/service/target/TextSecureServer-4.97.jar server ~/signal/Signal-Server/config.yml &>/dev/null &"
